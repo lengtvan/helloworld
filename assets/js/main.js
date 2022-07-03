bdd6930ca5dda195446f3e4f66765239d3fc3366
lengtvan <leng.tvan@gmail.com>
2022-07-03 21:40:52 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,53 +89,6 @@ document.addEventListener("DOMContentLoaded", () => {
       prevEl: ".custom-swiper-button-prev",
     },
   });
-  /**
-   * Change theme
-   */
-  // function changeTheme(id) {
-  //   const theme = document.getElementsByTagName("link")[11];
-  //   switch (id) {
-  //     case "typography1":
-  //       theme.href = "assets/css/Medieval.css";
-  //       break;
-  //     case "typography2":
-  //       theme.href = "assets/css/19thCentury.css";
-  //       break;
-  //     case "typography3":
-  //       theme.href = "assets/css/Firsthalf20th.css";
-  //       break;
-  //     case "typography4":
-  //       theme.href = "assets/css/Secondhalf20th.css";
-  //       break;
-  //     case "typography5":
-  //       theme.href = "assets/css/Late20th.css";
-  //       break;
-  //     case "typography6":
-  //       theme.href = "assets/css/2030.css";
-  //       break;
-  //   }
-  // }
-  // document.getElementById("typography1").addEventListener("click", () => {
-  //   changeTheme("typography1");
-  // });
-  // document.getElementById("typography2").addEventListener("click", () => {
-  //   changeTheme("typography2");
-  // });
-  // document.getElementById("typography3").addEventListener("click", () => {
-  //   changeTheme("typography3");
-  // });
-  // document.getElementById("typography4").addEventListener("click", () => {
-  //   changeTheme("typography4");
-  // });
-  // document.getElementById("typography5").addEventListener("click", () => {
-  //   changeTheme("typography5");
-  // });
-  // document.getElementById("typography6").addEventListener("click", () => {
-  //   changeTheme("typography6");
-  // });
-  /**
-   * Get metadata
-   */
 
   /**
    * Initiate glightbox
